4e0ce2cb1cf95234bc6630eeb99f051bab460d3b
MIN LI <minli@us.ibm.com>
2015-07-30 14:53:05 -0700

    minor

--- config/config.py
+++ config/config.py
@@ -27,7 +27,8 @@ SPARK_CONF_DIR = SPARK_HOME_DIR + "/conf"
 # For local clusters: "spark://%s:7077" % socket.gethostname()
 # For Yarn clusters: "yarn"
 # Otherwise, the default uses the specified EC2 cluster
-SPARK_CLUSTER_URL = open("/root/spark-ec2/cluster-url", 'r').readline().strip()
+SPARK_CLUSTER_URL = "spark://minli2.sl.cloud9.ibm.com:7077" 
+# SPARK_CLUSTER_URL = open("/root/spark-ec2/cluster-url", 'r').readline().strip()
 IS_YARN_MODE = "yarn" in SPARK_CLUSTER_URL
 IS_MESOS_MODE = "mesos" in SPARK_CLUSTER_URL
 
